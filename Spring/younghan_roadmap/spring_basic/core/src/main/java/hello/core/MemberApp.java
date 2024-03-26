c36ca1e0ad7436f2c25ccca5728356827079a3bc
yeom220 <yeom220@gmail.com>
2024-03-27 08:17:55 +0900

    스프링 핵심 원리-기본편> 순수자바->스프링으로 전환

--- Spring/younghan_roadmap/spring_basic/core/src/main/java/hello/core/MemberApp.java
+++ Spring/younghan_roadmap/spring_basic/core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.domain.Member;
 import hello.core.member.service.MemberService;
 import hello.core.member.service.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        // 순수 자바로 구현한 DI 컨테이너
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "spring", Grade.VIP);
 
--- Spring/younghan_roadmap/spring_basic/core/src/main/java/hello/core/OrderApp.java
+++ Spring/younghan_roadmap/spring_basic/core/src/main/java/hello/core/OrderApp.java
@@ -3,19 +3,27 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.domain.Member;
 import hello.core.member.service.MemberService;
-import hello.core.member.service.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // 순수 자바 DI 컨테이너 구현
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        // 스프링 컨테이너
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
