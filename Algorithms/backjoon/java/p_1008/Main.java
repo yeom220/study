dd7a1ed30832e0093fee28b28cea75d22bc88c11
yeom220 <yeom220@gmail.com>
2024-03-27 08:55:55 +0900

    알고리즘>백준 1008 10869 풀이

---
+++ Algorithms/backjoon/java/p_1008/Main.java
@@ -0,0 +1,13 @@
+package p_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double result = a/b;
+        System.out.println(result);
+    }
+}
