41e6ce27b4bc08a9f5fe260b67dbdb4d3637307f
yeom220 <yeom220@gmail.com>
2024-04-21 22:52:29 +0900

    스프링 MVC 1편>MVC 프레임워크 만들기>프론트 컨트롤러 V3, V4 정리

--- Spring/younghan_roadmap/spring_mvc_1/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ Spring/younghan_roadmap/spring_mvc_1/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -17,5 +18,15 @@ public class MyView {
     public void render(HttpServletRequest req, HttpServletResponse reps) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
         dispatcher.forward(req, reps);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse reps) throws ServletException, IOException {
+        modelToRequestAttribute(model, req);
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, reps);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+        model.forEach((key, value) -> req.setAttribute(key, value));
     }
 }
