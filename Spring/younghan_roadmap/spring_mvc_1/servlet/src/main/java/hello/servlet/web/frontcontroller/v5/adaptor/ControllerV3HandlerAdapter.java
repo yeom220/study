ccd8bda161aefe992a5597cfd7cc910a252e7218
yeom220 <yeom220@gmail.com>
2024-04-22 14:35:33 +0900

    스프링 MVC 1편>MVC 프레임워크 만들기>프론트 컨트롤러 V5 정리

---
+++ Spring/younghan_roadmap/spring_mvc_1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adaptor/ControllerV3HandlerAdapter.java
@@ -0,0 +1,36 @@
+package hello.servlet.web.frontcontroller.v5.adaptor;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws ServletException, IOException {
+
+        Map<String, String> paramMap = createParamMap(req);
+        ControllerV3 controller = (ControllerV3) handler;
+        return controller.process(paramMap);
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+
+}
