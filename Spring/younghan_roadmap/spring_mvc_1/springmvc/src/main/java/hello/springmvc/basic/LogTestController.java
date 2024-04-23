a899cca0e75c4590f36448b9d87c2e2fd81e395b
yeom220 <yeom220@gmail.com>
2024-04-23 21:58:54 +0900

    스프링 MVC 1편>스프링 MVC 기본 기능>HTTP 요청 파라미터 정리

---
+++ Spring/younghan_roadmap/spring_mvc_1/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    @GetMapping("/log-test")
+    public String logTest(){
+
+        String name = "Spring MVC";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        // 로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
+        // log.debug("String concat log=" + name);
+        return "ok";
+    }
+}
