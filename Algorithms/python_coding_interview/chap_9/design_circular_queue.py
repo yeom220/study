163951e39f6c1621245e58ebafddd6534dcaf79f
yeom220 <yeom220@gmail.com>
2024-04-25 09:51:49 +0900

    알고리즘>파이썬 코딩 인터뷰>9장 스택, 큐>24~25번 문제 풀이

---
+++ Algorithms/python_coding_interview/chap_9/design_circular_queue.py
@@ -0,0 +1,60 @@
+# Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle, and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+#
+# One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+#
+# Implement the MyCircularQueue class:
+#
+# MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+# int Front() Gets the front item from the queue. If the queue is empty, return -1.
+# int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+# boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+# boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+# boolean isEmpty() Checks whether the circular queue is empty or not.
+# boolean isFull() Checks whether the circular queue is full or not.
+# You must solve the problem without using the built-in queue data structure in your programming language.
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = [None] * k
+        self.maxlen = k
+        self.front, self.rear = 0, 0
+
+    # 리어 포인터 이동
+    def enQueue(self, value: int) -> bool:
+        if self.q[self.rear] is None:
+            self.q[self.rear] = value
+            self.rear = (self.rear + 1) % self.maxlen
+            return True
+        else:
+            return False
+
+    # 프론트 포인터 이동
+    def deQueue(self) -> bool:
+        if self.q[self.front] is None:
+            return False
+        else:
+            self.q[self.front] = None
+            self.front = (self.front + 1) % self.maxlen
+            return True
+
+    def Front(self) -> int:
+        return -1 if self.q[self.front] is None else self.q[self.front]
+
+    def Rear(self) -> int:
+        return -1 if self.q[self.rear - 1] is None else self.q[self.rear - 1]
+
+    def isEmpty(self) -> bool:
+        return self.front == self.rear and self.q[self.front] is None
+
+    def isFull(self) -> bool:
+        return self.front == self.rear and self.q[self.rear] is not None
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()0
