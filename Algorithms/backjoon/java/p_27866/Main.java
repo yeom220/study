88a243a0e658dfefe52c41e2ba8e162b419fa0bb
yeom220 <yeom220@gmail.com>
2024-04-26 07:40:30 +0900

    알고리즘>백준 27866 풀이

---
+++ Algorithms/backjoon/java/p_27866/Main.java
@@ -0,0 +1,13 @@
+package p_27866;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = sc.nextInt();
+        char[] chars = str.toCharArray();
+        System.out.println(chars[n - 1]);
+    }
+}
