c5eb4faea9275611bcf096b2c84220dba2840e7b
yeom220 <yeom220@gmail.com>
2024-04-30 17:36:30 +0900

    스프링 MVC 2편>로그인 처리1 - 쿠키, 세션 정리

---
+++ Spring/younghan_roadmap/spring_mvc_2/login/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,41 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "member/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("errorsL {}", bindingResult);
+            return "member/addMemberForm";
+        }
+
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
