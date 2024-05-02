88900ac5404f6ab2c8a1938a4472fd4a0569e09a
yeom220 <yeom220@gmail.com>
2024-05-02 09:54:45 +0900

    알고리즘>파이썬 코딩 인터뷰>12장 그래프>35번 문제 풀이

--- Algorithms/python_coding_interview/chap_12/35_combinations.py
+++ Algorithms/python_coding_interview/chap_12/35_combinations.py
@@ -1,6 +1,7 @@
 # Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
 #
 # You may return the answer in any order.
+import itertools
 from typing import List
 
 # Example 1:
@@ -21,24 +22,28 @@ expected = [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        results = []
+        # 1. DFS로 k개 조합 생성
+        # results = []
+        #
+        # def dfs(elements, start: int, k: int):
+        #     print('start, k', start, k)
+        #     print('elements', elements)
+        #
+        #     if k == 0:
+        #         results.append(elements[:])
+        #         print('results', results)
+        #
+        #     for i in range(start, n + 1):
+        #         print('i, k', i, k)
+        #         elements.append(i)
+        #         dfs(elements, i + 1, k - 1)
+        #         print(elements.pop())
+        #
+        # dfs([], 1, k)
+        # return results
 
-        def dfs(elements, start: int, k: int):
-            print('start, k', start, k)
-            print('elements', elements)
-
-            if k == 0:
-                results.append(elements[:])
-                print('results', results)
-
-            for i in range(start, n + 1):
-                print('i, k', i, k)
-                elements.append(i)
-                dfs(elements, i + 1, k - 1)
-                print(elements.pop())
-
-        dfs([], 1, k)
-        return results
+        # 2. itertools 모듈 사용
+        return list(map(list, itertools.combinations(range(1, n + 1), k)))
 
 
 s = Solution()
