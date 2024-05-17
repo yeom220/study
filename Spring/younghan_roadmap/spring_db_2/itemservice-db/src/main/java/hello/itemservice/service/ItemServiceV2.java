9fc019320d010555b0fd5e705630bd926cfd5538
yeom220 <yeom220@gmail.com>
2024-05-17 11:01:27 +0900

    스프링 DB 2편>데이터 접근 기술 - 활용방안 정리

---
+++ Spring/younghan_roadmap/spring_db_2/itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV2.java
@@ -0,0 +1,46 @@
+package hello.itemservice.service;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemServiceV2 implements ItemService {
+
+    private final ItemRepositoryV2 itemRepository;
+    private final ItemQueryRepositoryV2 queryRepository;
+
+
+    @Override
+    public Item save(Item item) {
+        return itemRepository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = itemRepository.findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemRepository.findById(id);
+    }
+
+    @Override
+    public List<Item> findItems(ItemSearchCond cond) {
+        return queryRepository.findAll(cond);
+    }
+}
