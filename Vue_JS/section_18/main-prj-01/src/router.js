6f52f457c96ba1e549aff54dd5c0490ec7cab820
yeom220 <yeom220@gmail.com>
2024-06-04 10:00:55 +0900

    Vue-완벽 가이드>섹션 18:Vue 앱 최적화 및 배포하기 정리

---
+++ Vue_JS/section_18/main-prj-01/src/router.js
@@ -0,0 +1,71 @@
+// import { defineAsyncComponent } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
+
+// const CoachDetail = defineAsyncComponent(() =>
+//   import('./pages/coaches/CoachDetail.vue')
+// );
+// const CoachRegistration = defineAsyncComponent(() =>
+//   import('./pages/coaches/CoachRegistration.vue')
+// );
+// const ContactCoach = defineAsyncComponent(() =>
+//   import('./pages/requests/ContactCoach.vue')
+// );
+// const RequestsReceived = defineAsyncComponent(() =>
+//   import('./pages/requests/RequestsReceived.vue')
+// );
+// const UserAuth = defineAsyncComponent(() =>
+//   import('./pages/auth/UserAuth.vue')
+// );
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+
+const router = createRouter({
+  // mode: 'history',
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      props: true,
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }], // /coaches/c1/contact
+    },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
+});
+
+export default router;
