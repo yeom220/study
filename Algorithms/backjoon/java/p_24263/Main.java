e318fdda3614c19384dbc3399e9455be35b84886
yeom220 <yeom220@gmail.com>
2024-06-06 22:33:22 +0900

    알고리즘>백준 24263 풀이

---
+++ Algorithms/backjoon/java/p_24263/Main.java
@@ -0,0 +1,11 @@
+package p_24263;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
