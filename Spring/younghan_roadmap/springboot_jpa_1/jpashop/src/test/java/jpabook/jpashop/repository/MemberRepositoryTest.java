78955fe587d08259e882df156b019bcb9f7ed394
yeom220 <yeom220@gmail.com>
2024-07-21 23:48:06 +0900

    스프링부트 JPA 1편>도메인 분석 설계>엔티티 클래스 개발2까지 정리

---
+++ Spring/younghan_roadmap/springboot_jpa_1/jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void testMember() {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long saveId = memberRepository.save(member);
+
+        // when
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);    // JPA 엔티티 동일성 보장
+    }
+}
